797615c0efbddc7cd9ae2db3676b2303ce757528
shubhampawar0901 <shubhampawar09011998@gmail.com>
2023-12-06 09:39:42 +0530

    localstorage getItem setItem

---
+++ excercises/LocalStorage_20231205/index.js
@@ -0,0 +1,8 @@
+// Assignments
+// 9.1: Create two variables myHobby , age . Now set their value in local storage (according to your hobby and age).
+
+// After setting also get value from local storage and display their values in console.
+
+localStorage.setItem("porgi kon", "moye moye");
+localStorage.setItem("barki", 21);
+localStorage.setItem('porgi kon',JSON.stringify({"nav":"Mohini"}));
